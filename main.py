54d0336fb37091749951cc4524b62773cd754492
saibabaswamisamarth-boop <saibabaswamisamarth@gmail.com>
2026-01-22 18:32:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,8 +56,7 @@ def intraday_data():
             b1 = process_intraday_breakout(symbol, data)
             if b1:
                 if symbol not in DAY_STATE["breakout"]:
-                    if len(DAY_STATE["breakout"]) < 10:
-                        DAY_STATE["breakout"][symbol] = b1
+                    DAY_STATE["breakout"][symbol] = b1
                 else:
                     DAY_STATE["breakout"][symbol].update(b1)
 
@@ -65,29 +64,34 @@ def intraday_data():
             b2 = process_intraday_boost(symbol, data)
             if b2:
                 if symbol not in DAY_STATE["boost"]:
-                    if len(DAY_STATE["boost"]) < 10:
-                        DAY_STATE["boost"][symbol] = b2
+                    DAY_STATE["boost"][symbol] = b2
                 else:
                     DAY_STATE["boost"][symbol].update(b2)
 
         except:
             continue
 
-    breakout_list = sorted(
+    # ---------------- TOP 10 LOCKING LOGIC ----------------
+
+    breakout_sorted = sorted(
         DAY_STATE["breakout"].values(),
         key=lambda x: x["move_pct"],
         reverse=True
     )
 
-    boost_list = sorted(
+    boost_sorted = sorted(
         DAY_STATE["boost"].values(),
         key=lambda x: x["r_factor"],
         reverse=True
     )
 
+    # फक्त top 10 memory मध्ये ठेव
+    DAY_STATE["breakout"] = {x["symbol"]: x for x in breakout_sorted[:10]}
+    DAY_STATE["boost"] = {x["symbol"]: x for x in boost_sorted[:10]}
+
     return {
-        "breakout": breakout_list,
-        "boost": boost_list,
+        "breakout": list(DAY_STATE["breakout"].values()),
+        "boost": list(DAY_STATE["boost"].values()),
         "time": datetime.now(IST).strftime("%I:%M:%S %p")
     }
 
